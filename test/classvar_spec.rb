43d982553ed2f6d4cbe7e729a1f278785d5abdc1
Justin Love <hg@JustinLove.name>
2008-12-29 22:21:31 -0600

    tests written wrong; test for variable creation
    --HG--
    branch : plato

--- test/classvar_spec.rb
+++ test/classvar_spec.rb
@@ -8,7 +8,7 @@ describe "ClassVars" do
     end
 
     it "should have cv" do
-      @obj.respond_to? :cv
+      @obj.should be_respond_to(:cv)
     end
   end
   
@@ -18,11 +18,16 @@ describe "ClassVars" do
     end
   
     it "should have cv" do
-      @class.respond_to? :cv
+      @class.should be_respond_to(:cv)
     end
     
     it "should have psuedo_class_var" do
-      @class.respond_to? :psuedo_class_var
+      @class.should be_respond_to(:psuedo_class_var)
+    end
+    
+    it "should create variables" do
+      @class.psuedo_class_var :barney
+      @class.cv.should be_respond_to(:barney)
     end
   end
 end
