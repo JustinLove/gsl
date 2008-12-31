ca0e65a4da7d63786df37a3733a96d740a398eaf
Justin Love <hg@JustinLove.name>
2008-12-30 22:40:38 -0600

    argument error in path helper
    --HG--
    branch : plato

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
-def libs(*args)
+def libs(args)
   args.each do |file|
     require File.join(File.dirname(__FILE__), '..', 'lib', file)
   end
 end
 
-def tests(*args)
+def tests(args)
   args.each do |file|
     require File.join(File.dirname(__FILE__), file)
   end
