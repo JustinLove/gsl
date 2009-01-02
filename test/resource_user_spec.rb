4ad2332a5597c79e4e2b69f0b7a4b67e9c5e4133
Justin Love <hg@JustinLove.name>
2009-01-01 21:29:05 -0600

    make resource, with range test
    --HG--
    branch : plato

--- test/resource_user_spec.rb
+++ test/resource_user_spec.rb
@@ -24,4 +24,28 @@ describe GSL::ResourceUser do
     @user.cv.components[:cubes].first.should be_kind_of(GSL::Component)
     @user.cv.resources.should include(:cubes)
   end
+  
+  it "makes resources" do
+    @user.class.make_resource(:score)
+    @user.cv.resources.should include(:score)
+    @user.score.should be_kind_of(GSL::Resource)
+    @user.set_to 5, :score
+    @user.score.value.should == 5
+  end
+
+  describe "make resources with bounds" do
+    before do
+      @user.class.make_resource(:spoons, 1..7)
+      @user.set_to 1, :spoons
+    end
+  
+    it "that accept valid values" do
+      @user.spoons.value.should == 1
+    end
+    
+    it "that reject invalid values " do
+      lambda{@user.set_to 8, :spoons}.should raise_error
+      @user.spoons.value.should <= 7
+    end
+  end
 end
