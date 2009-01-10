bfb7702fed7e96c5e6f629f333aafddd3529abcf
Justin Love <hg@JustinLove.name>
2009-01-10 16:16:19 -0600

    refactor colors
    --HG--
    branch : plato

--- test/player_spec.rb
+++ test/player_spec.rb
@@ -10,6 +10,7 @@ describe GSL::Player do
     @game = GSL::Game.new()
     @game.create_players(3)
     @object = @game.players.first
+    @colors = [:red, :green, :blue]
   end
 
   it_should_behave_like "well behaved objects"
@@ -24,16 +25,16 @@ describe GSL::Player do
   end
   
   it "picks a color" do
-    @object.pick_color :red, :green, :blue
-    [:red, :green, :blue].should include(@object.color)
+    @object.pick_color(*@colors)
+    @colors.should include(@object.color)
   end
   
   it "doesn't duplicate colors" do
     @game.players.each do |player|
-      player.pick_color :red, :green, :blue
+      player.pick_color(*@colors)
     end
     @game.players.map {|player| player.color.to_s}.sort.
-      should == ['red', 'green', 'blue'].sort
+      should == @colors.map {|c| c.to_s}.sort
   end
   
 end
