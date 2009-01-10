4c454bece37f0dc3ef1e0c0fbc24a13ade11411d
Justin Love <hg@JustinLove.name>
2009-01-10 11:24:25 -0600

    don't auto include module
    --HG--
    branch : plato

--- lib/gsl.rb
+++ lib/gsl.rb
@@ -1,6 +1,4 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'depends')
 GSL::depends_on %w{game}
 
-[GSL].each {|constant| include constant}
-
-Game.new(ARGV.shift) if ARGV.length > 0
+GSL::Game.new(ARGV.shift) if ARGV.length > 0
