0014d7bd7e65706df3eef71d6b673247ab0dbf91
Justin Love <hg@JustinLove.name>
2009-01-10 11:42:26 -0600

    spec component :in, actions
    --HG--
    branch : plato

--- test/component_spec.rb
+++ test/component_spec.rb
@@ -14,6 +14,21 @@ describe GSL::Component do
     @object.to_s.should be_kind_of(String)
   end
   
+  it "responds to :in" do
+    @object.should be_respond_to(:in)
+  end
+  
+  it "sets actions" do
+    GSL::Component.define_action("widget", lambda {$ran = true})
+    @object.to_proc.should_not be_nil
+  end
+  
+  it "executes actions" do
+    $ran = false
+    @object.to_proc.call
+    $ran.should be_true
+  end
+
   describe "from hash" do
   end
   
