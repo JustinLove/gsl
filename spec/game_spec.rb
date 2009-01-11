08b5e6207f19021245fe1061be67f77b83a8cafa
Justin Love <hg@JustinLove.name>
2009-01-10 20:38:43 -0600

    components and resources
    --HG--
    branch : plato

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -26,4 +26,24 @@ describe GSL::Game do
     @object.number_of_players 3..4
     @object.number_of_players.should == (3..4)
   end
+  
+  it "defines common components" do
+    @object.common_components :cubes => 5
+    @object.cv.components.keys.should include(:cubes)
+  end
+
+  it "defines player components" do
+    @object.player_components :pyramids => 5
+    GSL::Player.cv.components.keys.should include(:pyramids)
+  end
+  
+  it "defines common resources" do
+    @object.common_resource :nuts
+    @object.cv.resources.should include(:nuts)
+  end
+  
+  it "defines player resources" do
+    @object.player_resource :seeds
+    GSL::Player.cv.resources.should include(:seeds)
+  end
 end
