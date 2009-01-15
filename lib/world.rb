0b0afe87957f1a03ace846902126946dfee3aaff
Justin Love <hg@JustinLove.name>
2009-01-15 09:33:33 -0600

    basic object tests
    --HG--
    branch : plato

--- lib/world.rb
+++ lib/world.rb
@@ -1,8 +1,14 @@
 module GSL
   module World
     class State
+      def to_s
+        "State #{object_id}"
+      end
     end
     class View
+      def to_s
+        "View"
+      end
     end
   end
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,14 +1,26 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 libs %w{world}
 
+class GSL::World::State
+  include Tattler
+end
+
+class GSL::World::View
+  include Tattler
+end
+
 describe GSL::World::State do
   before do
     @object = GSL::World::State.new
   end
+  
+  it_should_behave_like "well behaved objects"
 end
 
 describe GSL::World::View do
   before do
     @object = GSL::World::View.new
   end
+
+  it_should_behave_like "well behaved objects"
 end
