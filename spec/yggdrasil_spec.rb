e383ab58546de81b8952f586202799c595b65b2b
Justin Love <hg@JustinLove.name>
2009-02-19 21:16:58 -0600

    wildcard yggdrasil tests
    --HG--
    branch : plato

--- spec/yggdrasil_spec.rb
+++ spec/yggdrasil_spec.rb
@@ -1,7 +1,2 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
-tests %w{
-  yggdrasil/state_spec
-  yggdrasil/world_spec
-  yggdrasil/passport_spec
-  yggdrasil/citizen_spec
-}
+Dir.glob(File.join(File.dirname(__FILE__), 'yggdrasil', '*.rb')).
+  each {|f| require f}
