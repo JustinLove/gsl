c9a48e02689a6c5f57cb43c1e13bfa6c38bf2f32
Justin Love <hg@JustinLove.name>
2009-02-27 19:08:41 -0600

    game should'nt have out of money error any more
    --HG--
    branch : plato

--- spec/gsl/waste_spec.rb
+++ spec/gsl/waste_spec.rb
@@ -3,10 +3,6 @@ libs %w{gsl/game}
 
 describe "Industrial Waste" do
   it "runs" do
-    begin
-      GSL::Game.new(file "waste.rb")
-    rescue GSL::Resource::Insufficient
-      pending "don't let it run out of money"
-    end
+    GSL::Game.new(file "waste.rb")
   end
 end
