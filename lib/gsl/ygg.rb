efb6c171b3bf05ccedacd38001c5d3901e316854
Justin Love <hg@JustinLove.name>
2009-04-09 22:17:03 -0500

    tracing slows execution
    --HG--
    branch : plato

--- lib/gsl/ygg.rb
+++ lib/gsl/ygg.rb
@@ -2,7 +2,7 @@ require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'yggdrasil')
 
 module Yggdrasil
   class State
-    include Tracing
+    #include Tracing
     #include Logging
     include ReadCache
   end
