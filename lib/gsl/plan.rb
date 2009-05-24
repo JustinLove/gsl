0814aa8d030977390d9a798c50b77d6e4330785f
Justin Love <hg@JustinLove.name>
2009-05-24 16:46:15 -0500

    initializer
    --HG--
    branch : plato

--- lib/gsl/plan.rb
+++ lib/gsl/plan.rb
@@ -1,5 +1,18 @@
 module GSL
   class Plan
+    def initialize(_who, _what, &_how)
+      super()
+      @who = _who
+      @what = _what
+      @how = _how
+      s = @what.to_s
+      if (s[0,1] == '#')
+        @why = "#{@what.class}"
+      else
+        @why = "#{s}"
+      end
+    end
+    
     def to_s
       "#{self.class} on #{@why}"
     end
--- spec/gsl/plan_spec.rb
+++ spec/gsl/plan_spec.rb
@@ -1,13 +1,24 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
-libs %w{gsl/plan}
+libs %w{gsl/plan gsl/ygg}
 
 class GSL::Plan
   include Tattler
 end
 
+class Ground
+  extend Yggdrasil::Citizen::Class
+  
+  def initialize
+    @world = Yggdrasil::World.new
+    @world.state.name = "root"
+    super
+  end
+end
+
 describe GSL::Plan do
   before do
-    @object = GSL::Plan.new()
+    @ground = Ground.new
+    @object = GSL::Plan.new(@ground, [])
   end
   
   it_should_behave_like "well behaved objects"
