a529cb7111a9e332ae0788528861b2087a44d5b1
Justin Love <hg@JustinLove.name>
2009-06-23 15:15:20 -0500

    history switch framework and testing framework
    --HG--
    branch : plato

--- lib/gsl/historian.rb
+++ lib/gsl/historian.rb
@@ -3,5 +3,16 @@ module GSL
     def to_s
       "History:"
     end
+    
+    def record(what)
+      case (what)
+      when Resource; record_resource(what);
+      else; raise "Can't record #{what.class}"
+      end
+    end
+    
+    def record_resource(what)
+      p what.value
+    end
   end
 end
--- spec/gsl/historian_spec.rb
+++ spec/gsl/historian_spec.rb
@@ -1,14 +1,33 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
-libs %w{gsl/historian}
+libs %w{gsl/historian gsl/resource_user}
 
 class GSL::Historian
   include Tattler
 end
 
+class User
+  include GSL::ResourceUser
+  include Yggdrasil::Citizen::Class
+
+  def initialize
+    @world = Yggdrasil::World.new
+    @world.state.name = "root"
+    super
+  end
+  
+  make_resource :cheese
+end
+
 describe GSL::Historian do
   before do
     @object = GSL::Historian.new
+    @user = User.new
+    @user.set 0, :cheese
   end
   
   it_should_behave_like "well behaved objects"
+  
+  it "records history" do
+    @object.record(@user.cheese)
+  end
 end
